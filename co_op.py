04978677b029a92449ecb60581738ace0d40d578
Dylachee <saaakbaevv@gmail.com>
2023-03-09 19:05:40 +0600

    scs

--- co_op.py
+++ co_op.py
@@ -2,3 +2,30 @@
 
 def reson():
     print("Мои условия очень просто я хочу что бы Мадара стал президентом!")
+
+"""
+heello@heello-HP-ProBook-450-G2:~/co_op$ git init
+Reinitialized existing Git repository in /home/heello/co_op/.git/
+heello@heello-HP-ProBook-450-G2:~/co_op$ git remote add origin git@github.com:kermitlafrog61/test_project.git
+heello@heello-HP-ProBook-450-G2:~/co_op$ git add .
+heello@heello-HP-ProBook-450-G2:~/co_op$ git commit -m Madara
+[master (root-commit) c4f232d] Madara
+ 1 file changed, 4 insertions(+)
+ create mode 100644 co_op.py
+heello@heello-HP-ProBook-450-G2:~/co_op$ git status 
+On branch master
+nothing to commit, working tree clean
+heello@heello-HP-ProBook-450-G2:~/co_op$ git branch 
+HEAD     master   
+heello@heello-HP-ProBook-450-G2:~/co_op$ git push -u origin master 
+Enumerating objects: 3, done.
+Counting objects: 100% (3/3), done.
+Delta compression using up to 4 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 330 bytes | 330.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+To github.com:kermitlafrog61/test_project.git
+ * [new branch]      master -> master
+Branch 'master' set up to track remote branch 'master' from 'origin'.
+heello@heello-HP-ProBook-450-G2:~/co_op$ 
+"""
